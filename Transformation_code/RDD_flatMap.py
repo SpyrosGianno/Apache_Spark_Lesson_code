d7d3d380acb2a0ed68163db192124bbe5e072866
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-16 20:54:40 +0200

    Update RDD_flatMap.py

--- Transformation_code/RDD_flatMap.py
+++ Transformation_code/RDD_flatMap.py
@@ -7,6 +7,9 @@ students_data ="Giorgos A ,Elpida A,Giannis c,Aggelos A,Marija B,Anna A".split("
 students_rdd = spark.sparkContext.parallelize(students_data)
 students_rdd.collect()
 
-
+#Efarmozoume thn sinartish flatMap()
 students = students_rdd.flatMap(lambda x: x.split(" "))
 students.collect()
+# mas epistrefei to arithmo twn gramatwn 
+students.count()
+students_rdd.count()
