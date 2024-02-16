a8ce1757f92907dd9c6684c56d597ece61f96476
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-16 02:47:54 +0200

    Update Rdd_Dataframe.py

--- DataFrame_code/Rdd_Dataframe.py
+++ DataFrame_code/Rdd_Dataframe.py
@@ -2,6 +2,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Rdd_Dataframe").getOrCreate()
 
-df = spark.sparkContext.createDataFrame([("Leonidas",15,20),("Maria",18,12),("Thanasis",12,19)]).toDF(["Name","Grade","number"])
+data = [("Leonidas",15,20),("Maria",18,12),("Thanasis",12,19)]
+header =["Name","Grade","number"]
+
+df = spark.createDataFrame(data,header)
 
 df.show()
