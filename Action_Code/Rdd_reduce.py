3609341b46bf266a2f1a76c4ac3aa0030c862730
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-17 20:56:50 +0200

    Update Rdd_reduce.py

--- Action_Code/Rdd_reduce.py
+++ Action_Code/Rdd_reduce.py
@@ -8,8 +8,10 @@ data = ([1500,2000,150,80,1800,3500,4200,1900,3000,1500,900,750])
 rdCost = spark.sparkContext.parallelize(data)
 rdCost.collect()
 
+#pragmatopoiei athrisma twn stixeiwn tou RDD
 yearKerd = rdCost.reduce(lambda poso1,poso2: poso1+poso2)
 print(yearKerd)
 
+#ypologizei to sinoliko poso twn apodixewn pou tha prepei na mazepsei
 Apodixeis = yearKerd*10/100
 print(Apodixeis)
