aab87f04fe6d2102bb0c85e99bcc9002eafbca96
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-17 12:41:59 +0200

    Create Rdd_reduce.py

---
+++ Action_Code/Rdd_reduce.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Rdd_reduce").getOrCreate()
+
+data = ([1500,2000,150,80,1800,3500,4200,1900,3000,1500,900,750])
+
+rdCost = spark.sparkContext.parallelize(data)
+rdCost.collect()
+
+yearKerd = rdCost.reduce(lambda poso1,poso2: poso1+poso2)
+yearScost = rdCost.reduce(lambda poso1,poso2: poso1+poso2*10%1)
+
+
+print(yearKerd)
+print(yearScost)
