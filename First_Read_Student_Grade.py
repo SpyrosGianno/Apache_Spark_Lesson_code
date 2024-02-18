5c401e07a939c48462809db517a0195299bab29a
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-18 13:26:25 +0200

    Update First_Read_Student_Grade.py

--- First_Read_Student_Grade.py
+++ First_Read_Student_Grade.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
-url ="https://github.com/SpyrosGianno/Apache_Spark_Lesson_code/blob/0461dad0e6a3d8ef4b95d8f71ed8783a642841f5/Student_Grades_tst.csv"
 spark = SparkSession.builder.appName("StudentGrades").getOrCreate()
 
-df = spark.read.csv(url,header=True,inferSchema=True)
+df = spark.read.csv(/home/ubuntu/Student_Grades_tst.csv,header=True,inferSchema=True)
 
 df.show()
