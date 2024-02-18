5a1b4032f0cea4f3b96dde1f3b9e9313382e033c
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-18 17:35:43 +0200

    Update CreateDataFrame.py

--- SQL_DataFrame_code/CreateDataFrame.py
+++ SQL_DataFrame_code/CreateDataFrame.py
@@ -1,1 +1,13 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("count_look").getOrCreate()
+
+row = ([(1258,"Eleni","Mathimatika", 7), (2258,"Nikos","Glossa",8), (3582,"Giannis","Mathimatik",5),
+                  (4582,"Maria","Istoria",6),(6584,"Kostas","Glossa", 8), (7854,"Leonidas","Mathimatika", 7), 
+                  (8898,"Marios","Mathimatika", 6), (9825,"Nikoleta","Glossa", 9),(8574,"Nikoleta","Mathimatika", 9)])
+
+colums = ["id","name","Math","grade"]
+
+stdGrade = spark.createDataFrame(row,colums)
+
+stdGrade.show()
