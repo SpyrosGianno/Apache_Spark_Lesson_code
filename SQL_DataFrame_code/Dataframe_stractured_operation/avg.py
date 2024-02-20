a6c3b43bb2efdd780b54775f24ffa6f6f83c4dc0
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-21 00:06:39 +0200

    Update avg.py

--- SQL_DataFrame_code/Dataframe_stractured_operation/avg.py
+++ SQL_DataFrame_code/Dataframe_stractured_operation/avg.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("StudentGrades").getOrCreate()
+spark = SparkSession.builder.appName("avg").getOrCreate()
 
 df = spark.read.csv("Student_Grades_tst.csv",header=True,inferSchema=True)
 df.show()
