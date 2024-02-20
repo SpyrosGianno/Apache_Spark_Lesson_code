d35caef0ec1abec3fbf6f49ef5a5511717c8154b
SpyrosGianno <159817446+SpyrosGianno@users.noreply.github.com>
2024-02-20 13:09:16 +0200

    Update groupby.py

--- SQL_DataFrame_code/Dataframe_stractured_operation/groupby.py
+++ SQL_DataFrame_code/Dataframe_stractured_operation/groupby.py
@@ -1,10 +1,14 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType
-spark = SparkSession.builder.appName("limit").getOrCreate()
 
-dfRead = spark.read.csv("sallary.csv", header=True)
+spark = SparkSession.builder.appName("groupby").getOrCreate()
+
+personSallary = ([(125844, "Petros", "Iakovidis", 5000), (115485, "Giannis", "Leonidou",3000), (11585, "Maria", "Giannaki",1500), (138781, "Tasos", "Petropoulos",2000), (12595, "Nikoleta", "Panou",1800), (115485, "Giannis", "Leonidou",3000), (125844, "Petros", "Iakovidis", 5000), (185841, "Panagiota", "Manolou", 1500)])
+
+colums = ["id", "firstname", "lastname", "salary"]
+
+dfRead = spark.createDataFrame(personSallary, colums)
 dfRead.show()
 
 
-dfGrp = dfRead.groupBy(dfRead.columns[3])
-dfGrp.show
+dfGrp = dfRead.groupBy(dfRead.columns[3]).sum(dfRead.columns[3])
+dfGrp.show()
